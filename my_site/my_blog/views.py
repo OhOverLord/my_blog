5092dc6a7311d9a38ca301b54cfe4dfb289d068e
Leonid <ski20173@yandex.ru>
2020-10-25 21:26:41 +0300

    upgrade blog

--- my_site/my_blog/views.py
+++ my_site/my_blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'my_blog/post/list.html', context={'posts': posts})
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'my_blog/post/list.html', context={'page': page,'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published',publish__year=year, publish__month=month,publish__day=day)
-    return render(request,'blog/post/detail.html',{'post': post})
+    return render(request,'my_blog/post/detail.html',{'post': post})
