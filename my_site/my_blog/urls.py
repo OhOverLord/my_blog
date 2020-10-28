7b411ef26fabbe98d95b66a9c1de4dc749508bf6
Leonid <ski20173@yandex.ru>
2020-10-29 01:03:35 +0300

    Added sharing to post

--- my_site/my_blog/urls.py
+++ my_site/my_blog/urls.py
@@ -5,5 +5,6 @@ app_name = 'my_blog'
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>', post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<slug:post>', post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share')
 ]
