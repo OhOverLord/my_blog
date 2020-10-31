434b34519727c21b7349b8d4646caf89b90d6ec7
Leonid <ski20173@yandex.ru>
2020-10-31 22:19:09 +0300

    Added similar posts

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_blog.apps.MyBlogConfig'
+    'my_blog.apps.MyBlogConfig',
+    'taggit'
 ]
 
 MIDDLEWARE = [
