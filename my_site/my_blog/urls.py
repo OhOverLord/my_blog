ab4e406894de0d998efeed8f2e637cacb1e9bf3d
Leonid <ski20173@yandex.ru>
2020-11-16 01:01:18 +0300

    Added search

--- my_site/my_blog/urls.py
+++ my_site/my_blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>', post_detail, name='post_detail'),
     path('<int:post_id>/share/', post_share, name='post_share'),
-    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag')
+    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('search/', post_search, name='post_search'),
 ]
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
@@ -61,8 +62,10 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': '123',
     }
 }
 
